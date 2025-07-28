afc333402630cbb6945a68964b2fc732c5431215
Terd2543 <terdteykk@gmail.com>
2025-07-28 14:19:09 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // สำหรับ React 18
+import './index.css'; // ถ้ามีไฟล์ CSS ของ index
+import App from './App'; // นำเข้า App Component ของคุณ
+import reportWebVitals from './reportWebVitals'; // สำหรับวัดประสิทธิภาพ (ถ้ามี)
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App /> {/* คอมโพเนนต์ App ของคุณจะถูก Render ที่นี่ */}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
